dfea5ba20feeed3f060c63fa1ad36ce6516802b9
Purnachandra <pratap.careers@gmail.com>
2024-12-19 14:27:09 +0530

    added product card, loaded from API

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,7 +1,26 @@
+import { Link } from 'react-router-dom';
 import style from './Product.module.css';
 
-function Product() {
-  return <div></div>;
+function Product({ data }) {
+  return (
+    <div className={style.product}>
+      <Link to={`/${data.id}`}>
+        <div className={style.image}>
+          <img src={data.thumbnail} alt={data.title} />
+        </div>
+        <div className={style.data}>
+          <p className={style.title}>{data.title}</p>
+          <div className={style.data2}>
+            <p className={style.price}>${data.price}</p>
+            <p className={style.rating}>⭐ {data.rating}</p>
+          </div>
+        </div>
+      </Link>
+      <div className={style.btn}>
+        <button>Add To Cart</button>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,21 +1,53 @@
 import { useEffect, useState } from 'react';
 import style from './Products.module.css';
+import Product from '../product/Product';
 
 function Products() {
-  const [list, useList] = useState();
-  const [count, setCount] = useState(1);
+  const [list, setList] = useState([]);
+  const [skip, setSkip] = useState(0);
+  const [error, setError] = useState(false);
+  const [limit, setLimit] = useState(100);
+  const [loading, setLoading] = useState(false);
+  console.log(list);
   useEffect(() => {
     async function getData() {
       try {
-        const res = fetch('https://dummyjson.com/products');
+        setLoading(true);
+        const res = await fetch(
+          `https://dummyjson.com/products?limit=12&skip=${
+            skip * 12
+          }&select=title,price,title,rating,thumbnail`
+        );
+        const data = await res.json();
+        setList(d => [...d, ...data.products]);
       } catch (e) {
         console.error(e.message);
+        setError(e.message);
+      } finally {
+        setLoading(false);
       }
     }
-  }, [count]);
+    getData();
+  }, [skip]);
+
+  const loadMore = () => {
+    if (limit < skip * 12) {
+      setError('No more products to load');
+      return;
+    }
+    setSkip(k => k + 1);
+  };
+
   return (
     <div className={style.Products}>
-      <div> </div>
+      <div>
+        {list.map((c, i) => (
+          <Product data={c} key={i} />
+        ))}
+      </div>
+      <div className={style.loadBtn}>
+        <button onClick={loadMore}>Load More</button>
+      </div>
     </div>
   );
 }
